f1f1b236dd9a43c12121f74ec94a6669b220ed8b
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-19 13:49:50 -0400

    add simple get route in app.js

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Todo application." });
+});
 // npm run dev
 app.listen(process.env.PORT, () => console.log('app is running'));
