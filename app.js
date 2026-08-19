656e5d7711fe41bb1515a9b8dd59d0e32328a9f9
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-19 14:13:57 -0400

    display public in port

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Todo application." });
-});
+// Display page
+app.use(express.static('public'))
+
 // npm run dev
 app.listen(process.env.PORT, () => console.log('app is running'));
