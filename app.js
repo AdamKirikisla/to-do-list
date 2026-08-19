a97912f7a9177d08c2e0bfd737742355688703bb
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-19 13:44:33 -0400

    init app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+// npm run dev
+app.listen(process.env.PORT, () => console.log('app is running'));
