239bf9ffdac88f7f8d990d64810257b1522edc83
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-20 16:02:13 -0400

    add createTask in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
-import { getTasks } from './api.js';
+import { getTasks, createTask } from './api.js';
 import { renderTasks, taskCounter } from './render.js';
 
+const form = document.querySelector('#add-task-form');
 let tasks = [];
 
 async function loadTasks() {
@@ -13,4 +14,23 @@ async function loadTasks() {
   }
 }
 
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const title = form.task.value.trim();
+  const priority = form['priority-forms'].value;
+  const category = form['category-forms'].value;
+
+  if (!title || !priority || !category) return;
+
+  try {
+    const newTask = await createTask({ title, priority, category });
+    tasks.push(newTask);
+    renderTasks(tasks);
+    taskCounter(tasks);
+    form.reset();
+  } catch (error) {
+    console.log('Something went wrong:', error);
+  }
+});
 loadTasks();
