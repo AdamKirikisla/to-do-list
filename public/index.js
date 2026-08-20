ea376524cdee6a5b1e76972a3d13320e79e74f8c
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-20 14:29:46 -0400

    update loadTasks

--- public/index.js
+++ public/index.js
@@ -8,20 +8,9 @@ async function loadTasks() {
     }
     tasks = await response.json();
 
-    const ul = document.querySelector('#task-list');
-
-    if (tasks.length === 0) {
-      ul.innerHTML = "<li><p>No Data</p></li>";
-      return;
-    }
-
-    for (let task of tasks) {
-      // render each task next
-    }
+    renderTasks(tasks);
 
   } catch (error) {
     console.log('Something went wrong:', error);
   }
 }
-
-loadTasks()
