d8733e2caed7158766fced9040d0b44bb4ced8fc
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-20 13:54:34 -0400

    update front 'add task'

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  loadHTMLTable([])
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   loadHTMLTable([])
+// });
 
-function loadHTMLTable(data) {
-  const ul = document.querySelector('#task-list');
+// function loadHTMLTable(data) {
+//   const ul = document.querySelector('#task-list');
 
-  if (data.length === 0) {
-    ul.innerHTML = "<li><p>No Data</p></li>";
-    return;
-  }
-}
+//   if (data.length === 0) {
+//     ul.innerHTML = "<li><p>No Data</p></li>";
+//     return;
+//   }
+// }
