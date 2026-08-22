17e94c7aeaa672c926696fd44a6534451b422cc8
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-21 20:04:45 -0400

    add user table in SQL

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,20 @@
 CREATE DATABASE IF NOT EXISTS todo_app;
 USE todo_app;
 DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     category ENUM('work', 'personal', 'errands') NOT NULL DEFAULT 'work',
     priority ENUM('low', 'medium', 'high') NOT NULL DEFAULT 'medium',
-    is_done BOOLEAN DEFAULT FALSE
+    is_done BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
