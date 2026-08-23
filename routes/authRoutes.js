1f08b231d67c0877ff37efdaf6beee479d3dca5e
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-08-22 22:38:22 -0400

    add logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {registerUser, loginUser} = require('../controllers/authController');
+const {registerUser, loginUser, logoutUser} = require('../controllers/authController');
 
 
 
@@ -7,5 +7,6 @@ const authRouter = express.Router();
 
 authRouter.post('/register', registerUser)
 authRouter.post('/login', loginUser)
+authRouter.post('/logout', logoutUser)
 
 module.exports = authRouter;
